c2817d352c7dce24c917a1a07dba64a9f9514f39
JuliaGolban <julia.j.shcherban@gmail.com>
2022-09-13 18:50:19 +0300

    changed on class notify functions

--- src/js/notify-messages.js
+++ src/js/notify-messages.js
@@ -1,19 +1,27 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-function onFetchError() {
-  return Notify.failure(
-    'Sorry, there are no images matching your search query. Please try again.'
-  );
+export default class NotifyMessages {
+  constructor() {}
+
+  onFetchError() {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  }
+
+  onFinishPhotoCards() {
+    return Notify.info(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+
+  onTotalPhotoCards(total) {
+    return Notify.success(`Hooray! We found ${total} images.`);
+  }
 }
 
-function onFinishPhotoCards() {
-  return Notify.info(
-    "We're sorry, but you've reached the end of search results."
-  );
-}
-
-function onTotalPhotoCards({ totalHits }) {
-  return Notify.success(`Hooray! We found ${totalHits} images.`);
-}
-
-export { onFetchError, onFinishPhotoCards, onTotalPhotoCards };
+Notify.init({
+  info: {
+    position: 'center-bottom',
+  },
+});
